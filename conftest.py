e5d9d45aeb910093388402c1deb7af81e7d45f9e
Aleksandr Minin <dmb2006minin@yandex.ru>
2025-05-22 21:19:18 +0300

    added print('Браузер открыт') in conftest.py

--- conftest.py
+++ conftest.py
@@ -6,10 +6,12 @@ from selene import browser, be
 def screen_resolution():
     browser.config.window_height = 1024
     browser.config.window_width = 1920
-    yield
-    browser.quit()
+
 @pytest.fixture(scope="session")
 def browser_open():
+    print('Браузер открыт')
     browser.open('https://duckduckgo.com/')
-
+    yield
+    browser.quit()
+    print('Браузер закрыт')
 
