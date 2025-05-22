d5dd9b456165d3740c94e21c001edfbae3e75483
Aleksandr Minin <dmb2006minin@yandex.ru>
2025-05-22 18:06:52 +0300

    added fixture and test oen browser

--- conftest.py
+++ conftest.py
@@ -1,10 +1,15 @@
 import pytest
-from selene import browser
+from selene import browser, be
+
 
 @pytest.fixture(scope="session")
 def screen_resolution():
     browser.config.window_height = 1024
-    browser.config.window_width = 768
+    browser.config.window_width = 1920
     yield
     browser.quit()
+@pytest.fixture(scope="session")
+def browser_open():
+    browser.open('https://duckduckgo.com/')
+
 
