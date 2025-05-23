1fbafbfaa27852277de197f9fa5028da58f4e06d
Aleksandr Minin <dmb2006minin@yandex.ru>
2025-05-23 12:26:11 +0300

    added test unsearch

--- conftest.py
+++ conftest.py
@@ -1,13 +1,15 @@
 import pytest
 from selene import browser
 
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def test_browser_resolution():
-    browser.config.window_width = 1440
-    browser.config.window_height = 768
+    browser.config.window_width = 768
+    browser.config.window_height = 1440
 
-@pytest.fixture()
-def test_browser():
+@pytest.fixture(scope='session')
+def test_browser_wb():
     browser.open('https://www.wildberries.ru')
 
-
+@pytest.fixture(scope='session')
+def test_browser_dd():
+    browser.open('https://duckduckgo.com/')
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from selene import browser, be, have
 
-def test_find_search(test_browser_resolution, test_browser):
-    browser.element('[type="search"]').should(be.blank).type(have.text('star wars')).press_enter()
-    browser.element('[id="c213739936"]').click()
+def test_find_search(test_browser_resolution, test_browser_wb):
+    browser.element('[type="search"]').should(be.blank).type('star wars').press_enter()
+    browser.element('[id="c314332073"]').click()
+
+def test_find_unsearch(test_browser_resolution, test_browser_dd):
+    browser.element('[name="q"]').should(be.blank).type('ываолрфварпварпвапыва').press_enter()
+    browser.element('html').should(have.text('По запросу «ываолрфварпварпвапыва» ничего не найдено.'))
+
