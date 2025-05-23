d5fb1adfb3d3961149c3b3a093293182bc40cb6d
Aleksandr Minin <dmb2006minin@yandex.ru>
2025-05-23 11:48:26 +0300

    add fixture

--- conftest.py
+++ conftest.py
@@ -1,16 +1,13 @@
 import pytest
-
 from selene import browser
-print('hello')
 
-@pytest.fixture(scope="session")
-def screen_resolution():
-    browser.config.window_height = 1024
-    browser.config.window_width = 1920
-    yield
-    browser.quit()
-@pytest.fixture(scope="session")
-def browser_open():
-    browser.open('https://duckduckgo.com/')
+@pytest.fixture()
+def test_browser_resolution():
+    browser.config.window_width = 1440
+    browser.config.window_height = 768
+
+@pytest.fixture()
+def test_browser():
+    browser.open('https://www.wildberries.ru')
 
 
