63eb497e25d517fe04ccd10b67e389bcef1bfaea
Aleksandr Minin <dmb2006minin@yandex.ru>
2025-05-23 15:32:37 +0300

    chengt name test positive/negative and find text in wb

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from selene import browser, be, have
 
-def test_find_search(test_browser_resolution, test_browser_wb):
+def test_search_positive(test_browser_resolution, test_browser_wb):
     browser.element('[type="search"]').should(be.blank).type('star wars').press_enter()
     browser.element('[id="c314332073"]').click()
+    browser.element('html').should(have.text("Световой меч джедая игрушечный"))
 
-def test_find_unsearch(test_browser_resolution, test_browser_dd):
+def test_search_negative(test_browser_resolution, test_browser_dd):
     browser.element('[name="q"]').should(be.blank).type('ываолрфварпварпвапыва').press_enter()
     browser.element('html').should(have.text('По запросу «ываолрфварпварпвапыва» ничего не найдено.'))
-
